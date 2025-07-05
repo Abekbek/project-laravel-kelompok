cf8b8f00a72d381fadedf8ba69bb40b51fc9b7be
Abekbek <anonimwork69@gmail.com>
2025-07-05 15:04:33 +0700

    Hardiansyah: Perbaikan warna pada halaman

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -22,7 +22,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased dark">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-slate-950">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-slate-950">
             <div class="flex flex-col items-center">
                 <a href="/">
                     <x-application-logo class="w-20 h-20" />
@@ -30,9 +30,10 @@
                 <p class="mt-3 text-xl font-semibold text-gray-800 dark:text-gray-200">ETHERLIST</p>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-slate-900 shadow-md overflow-hidden border border-black">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-slate-900 shadow-md overflow-hidden border border-black">
                 {{ $slot }}
             </div>
+
         </div>
     </body>
 </html>
